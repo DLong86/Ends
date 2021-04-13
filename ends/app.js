b5934bb25824e5f45a66363d5730d974f9c9c3a1
DLong86 <74575072+DLong86@users.noreply.github.com>
2021-04-13 12:14:58 +0900

    Add files via upload

---
+++ ends/app.js
@@ -0,0 +1,22 @@
+// PICTURE SLIDERS
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    removeActiveClasses();
+    panel.classList.add('active');
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove('active');
+  });
+}
+
+// RESPONSIVE SIDE NAV
+document.querySelector('.toggle')
+
+
+
+
